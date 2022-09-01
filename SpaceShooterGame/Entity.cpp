c8039b89a2288572b467d77cce5fc11a9b0d1e4d
Krystian Sobczyk <krystian.sobczyk5642@wp.pl>
2022-09-01 13:35:56 +0200

    Entity class added

---
+++ SpaceShooterGame/Entity.cpp
@@ -0,0 +1,69 @@
+#include "Entity.h"
+
+extern float xBackgroundSize;
+extern float yBackgroundSize;
+
+Entity::Entity(std::string entityName, float xScale, float yScale, size_t textureNumber)
+{
+	setEntityName(entityName);
+	setScale(sf::Vector2f(xScale, yScale));
+	setTextureNumber(textureNumber);
+}
+
+Entity::Entity(const Entity& entity)
+{
+	setEntityName(entity.entityName);
+	setScale(entity.getScale());
+	setTextureNumber(entity.textureNumber);
+	entityTextures = entity.entityTextures;
+	setTexture(entityTextures[0].get());
+	setSize(entity.getSize());
+	setOrigin(entity.getOrigin());
+}
+
+Entity::~Entity()
+{
+}
+
+const std::string& Entity::getEntityName() const
+{
+	return entityName;
+}
+
+void Entity::setEntityName(const std::string& newEntityName)
+{
+	this->entityName = newEntityName;
+}
+
+const size_t& Entity::getTextureNumber() const
+{
+	return textureNumber;
+}
+
+void Entity::setTextureNumber(const size_t& newTextureNumber)
+{
+	if (newTextureNumber <= 0)
+	{
+		throw std::logic_error("textureNumber cannot be less or equal zero");
+	}
+	this->textureNumber = newTextureNumber;
+}
+
+const sf::Vector2f& Entity::getScale() const
+{
+	return scale;
+}
+
+void Entity::setScale(const sf::Vector2f& newScale)
+{
+	if (scale.x <= 0 && scale.y <= 0)
+	{
+		throw std::logic_error("scale cannot be less or equal zero");
+	}
+	this->scale = newScale;
+}
+
+const size_t& Entity::getTextureIndex() const
+{
+	return textureIndex;
+}
---
+++ SpaceShooterGame/Entity.h
@@ -0,0 +1,48 @@
+#ifndef  ENTITY_H
+#define  ENTITY_H
+
+#include "Libraries.h"
+
+class Entity : public sf::RectangleShape
+{
+protected:
+	//varaibles
+	std::shared_ptr<std::shared_ptr<sf::Texture>[]> entityTextures;
+	std::string entityName = "";
+
+	size_t textureNumber = 1;
+	size_t textureIndex = 0;
+
+	sf::Vector2f scale = sf::Vector2f(1, 1);
+
+	std::vector <std::string> texturePaths;
+
+	//pure virtual functions
+	virtual void loadEntityTexture() = 0;
+
+public:
+	//constructors and virtual destructor
+	Entity(std::string entityName = "",
+		float xScale = 1,
+		float yScale = 1,
+		size_t textureNumber = 1);
+	Entity(const Entity& entity);
+	virtual ~Entity();
+
+	//getters and setters
+	const std::string& getEntityName() const;
+	void setEntityName(const std::string& newEntityName);
+
+	const size_t& getTextureNumber() const;
+	void setTextureNumber(const size_t& newTextureNumber);
+
+	const sf::Vector2f& getScale() const;
+	void setScale(const sf::Vector2f& newScale);
+
+	const size_t& getTextureIndex() const;
+
+	//pure virtual functions
+	virtual void updateEntity() = 0;
+};
+
+#endif // !ENTITY_H
