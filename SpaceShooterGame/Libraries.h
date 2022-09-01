e06e713fa8c06baf64a19bf664a9e876e2aa3b5a
Krystian Sobczyk <krystian.sobczyk5642@wp.pl>
2022-09-01 13:48:49 +0200

    Libraries added

---
+++ SpaceShooterGame/Libraries.h
@@ -0,0 +1,20 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <Windows.h>
+#include <vector>
+#include <cmath>
+#include <memory>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <map>
+#include <sstream>
+
+#endif
